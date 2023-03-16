e0601b6007e279c708fd8578f37b715e754dceaf
Chris Ohk <utilforever@gmail.com>
2023-03-16 16:54:37 +0900

    feat: Update assignment 1-2

---
+++ 3 - Assignment/230316 - Rust Basic + Make Interpreter, Week 1/prob2/src/lib.rs
@@ -0,0 +1,94 @@
+#[derive(Debug)]
+pub enum CalculatorInput {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Value(i32),
+}
+
+pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
+    unimplemented!(
+		"Given the inputs: {:?}, evaluate them as though they were a Reverse Polish notation expression",
+		inputs,
+	);
+}
+
+#[cfg(test)]
+fn calculator_input(s: &str) -> Vec<CalculatorInput> {
+    s.split_whitespace()
+        .map(|s| match s {
+            "+" => CalculatorInput::Add,
+            "-" => CalculatorInput::Subtract,
+            "*" => CalculatorInput::Multiply,
+            "/" => CalculatorInput::Divide,
+            n => CalculatorInput::Value(n.parse().unwrap()),
+        })
+        .collect()
+}
+
+#[test]
+fn test_empty_input_returns_none() {
+    let input = calculator_input("");
+    assert_eq!(evaluate(&input), None);
+}
+
+#[test]
+fn test_simple_value() {
+    let input = calculator_input("10");
+    assert_eq!(evaluate(&input), Some(10));
+}
+
+#[test]
+fn test_simple_addition() {
+    let input = calculator_input("2 2 +");
+    assert_eq!(evaluate(&input), Some(4));
+}
+
+#[test]
+fn test_simple_subtraction() {
+    let input = calculator_input("7 11 -");
+    assert_eq!(evaluate(&input), Some(-4));
+}
+
+#[test]
+fn test_simple_multiplication() {
+    let input = calculator_input("6 9 *");
+    assert_eq!(evaluate(&input), Some(54));
+}
+
+#[test]
+fn test_simple_division() {
+    let input = calculator_input("57 19 /");
+    assert_eq!(evaluate(&input), Some(3));
+}
+
+#[test]
+fn test_complex_operation() {
+    let input = calculator_input("4 8 + 7 5 - /");
+    assert_eq!(evaluate(&input), Some(6));
+}
+
+#[test]
+fn test_too_few_operands_returns_none() {
+    let input = calculator_input("2 +");
+    assert_eq!(evaluate(&input), None);
+}
+
+#[test]
+fn test_too_many_operands_returns_none() {
+    let input = calculator_input("2 2");
+    assert_eq!(evaluate(&input), None);
+}
+
+#[test]
+fn test_zero_operands_returns_none() {
+    let input = calculator_input("+");
+    assert_eq!(evaluate(&input), None);
+}
+
+#[test]
+fn test_intermediate_error_returns_none() {
+    let input = calculator_input("+ 2 2 *");
+    assert_eq!(evaluate(&input), None);
+}
